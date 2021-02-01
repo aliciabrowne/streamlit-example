bd9c2e22a461df671b397b76c8e1eb33040f5a61
aliciabrowne <44396835+aliciabrowne@users.noreply.github.com>
2021-02-01 20:11:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ import streamlit as st
 
 st.title('Recommender System using Pairwise Preference')
 st.text('Here I will recommend songs to you based on your preference of a couple of songs.')
+uploaded_file = st.file_uploader("Choose an image", ["jpg","jpeg","png"])
